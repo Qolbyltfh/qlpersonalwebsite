ae6ed1574dab3e3906d8640a0fbcc2c5ecb3cc85
Qolbyltfh <qolbi07latifah@gmail.com>
2024-01-16 10:31:56 +0700

    ✨ empty LandingPage

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,12 +7,15 @@ import {
 } from "react-router-dom";
 
 import My404Component from '../pages/My404Component'
+import LandingPage from '../pages/LandingPage/index'
+
 
 const Routes = () => {
   return (
     <Router>
       <WrapperRoutes>
         <Route path="*" element={<My404Component />} />
+        <Route path="/" element={<LandingPage />} />
       </WrapperRoutes>
     </Router>
   );
