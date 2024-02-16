4c4c3886c5664b23c514642596110db9e99eae21
Qolbyltfh <qolbi07latifah@gmail.com>
2024-02-16 17:16:25 +0700

    💄 Projects

---
+++ src/pages/Project.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const Project = () => {
+    return (
+        <Layout>
+            
+        </Layout>
+    );
+};
+
+export default Project;
--- src/routes/index.js
+++ src/routes/index.js
@@ -10,6 +10,8 @@ import My404Component from '../pages/My404Component'
 import LandingPage from '../pages/LandingPage/index'
 import About from "../pages/About";
 import Resume from "../pages/Resume";
+import Project from "../pages/Project";
+
 const Routes = () => {
   return (
     <Router>
@@ -18,6 +20,7 @@ const Routes = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/about" element={<About />} />
         <Route path="/resume" element={<Resume />} />
+        <Route path="/projects" element={<Project />} />
       </WrapperRoutes>
     </Router>
   );
